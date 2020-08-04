abcef6c6f225155350fcfce6117e09ad67d63856
Miguel Angel R <migrmrz@gmail.com>
2020-08-04 12:46:44 -0500

    Minor adjustments

--- config.py
+++ config.py
@@ -2,19 +2,20 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 import os
 
+user = 'anticraista'
+scope = 'playlist-modify-private'
 client_id = os.environ.get("client_id")
 client_secret = os.environ.get("client_secret")
+redirect_uri = 'http://migrmrz.wordpress.com/'
 
 spotify_client_credentials = SpotifyClientCredentials(
     client_id,
     client_secret
 )
 
-scope = 'playlist-modify-private'
-
 user_token = util.prompt_for_user_token(
-    'anticraista',
+    user,
     scope,
     client_id,
     client_secret,
-    'http://migrmrz.wordpress.com/')
+    redirect_uri)
